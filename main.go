53f62a855de05c2164b9b8f423d8a0ca0658bbd7
tomiokashougo <tomiokasyogo@gmail.com>
2019-03-11 16:00:21 +0900

    add new graphql

--- main.go
+++ main.go
@@ -4,123 +4,71 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"log"
+	"net/http"
+	"os"
 
 	"github.com/graphql-go/graphql"
 )
 
-// Tutorial用のデータスキーマ
-type Tutorial struct {
-	ID       int
-	Title    string
-	Author   Author
-	Comments []Comment
-}
-
-type Author struct {
-	Name      string
-	Tutorials []int
-}
-
-type Comment struct {
-	Body string
-}
-
-func populate() []Tutorial {
-	author := &Author{Name: "Elliot Forbes", Tutorials: []int{1}}
-	tutorial := Tutorial{
-		ID:     1,
-		Title:  "Go GraphQL Tutorial",
-		Author: *author,
-		Comments: []Comment{
-			Comment{Body: "First Comment"},
+var q graphql.ObjectConfig = graphql.ObjectConfig{
+	Name: "query",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type:    graphql.ID,
+			Resolve: resolveID,
 		},
+		"name": &graphql.Field{
+			Type:    graphql.String,
+			Resolve: resolveName,
+		},
+	},
+}
+
+var schemaConfig graphql.SchemaConfig = graphql.SchemaConfig{
+	Query: graphql.NewObject(q),
+}
+
+// ここでスキーマを定義
+var schema, _ = graphql.NewSchema(schemaConfig)
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	r := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+
+	if r.HasErrors() {
+		fmt.Fprintf(os.Stderr, "An error is occured : %v", r.Errors)
 	}
 
-	var tutorials []Tutorial
-	tutorials = append(tutorials, tutorial)
+	j, _ := json.Marshal(r)
+	fmt.Printf("%s \n", j)
 
-	return tutorials
+	return r
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	bufBody := new(bytes.Buffer)
+	bufBody.ReadFrom(r.Body)
+	query := bufBody.String()
+
+	result := executeQuery(query, schema)
+	json.NewEncoder(w).Encode(result)
 }
 
 func main() {
+	query := "{ id }"
+	executeQuery(query, schema)
 
-	tutorials := populate()
-	// Schema
-	fields := graphql.Fields{
-		"tutorial": &graphql.Field{
-			Type: tutorialType,
-			// it's good form to add a description
-			// to each field.
-			Description: "Get Tutorial By ID",
-			// We can define arguments that allow us to
-			// pick specific tutorials. In this case
-			// we want to be able to specify the ID of the
-			// tutorial we want to retrieve
-			Args: graphql.FieldConfigArgument{
-				"id": &graphql.ArgumentConfig{
-					Type: graphql.Int,
-				},
-			},
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				// take in the ID argument
-				id, ok := p.Args["id"].(int)
-				if ok {
-					// Parse our tutorial array for the matching id
-					for _, tutorial := range tutorials {
-						if int(tutorial.ID) == id {
-							// return our tutorial
-							return tutorial, nil
-						}
-					}
-				}
-				return nil, nil
-			},
-		},
-		// this is our `list` endpoint which will return all
-		// tutorials available
-		"list": &graphql.Field{
-			Type:        graphql.NewList(tutorialType),
-			Description: "Get Tutorial List",
-			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-				return tutorials, nil
-			},
-		},
-	}
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-	schema, err := graphql.NewSchema(schemaConfig)
-	if err != nil {
-		log.Fatalf("failed to create new schema, error: %v", err)
-	}
+	query = "{ id,name}"
+	executeQuery(query, schema)
 
-	// Query
-	query := `
-{
-	list {
-		id
-		title
-		comments {
-			body
-		}
-		author {
-			Name
-			Tutorials
-		}
-	}
 }
-`
-	params := graphql.Params{Schema: schema, RequestString: query}
-	r := graphql.Do(params)
-	if len(r.Errors) > 0 {
-		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
-	}
-	rJSON, _ := json.Marshal(r)
-	var buf bytes.Buffer
-	err = json.Indent(&buf, []byte(rJSON), "", "  ")
-	if err != nil {
-		panic(err)
-	}
-	indentJson := buf.String()
-	fmt.Printf("%s \n", indentJson) // {“data”:{“hello”:”world”}}
+
+func resolveID(p graphql.ResolveParams) (interface{}, error) {
+	return 1, nil
+}
+
+func resolveName(p graphql.ResolveParams) (interface{}, error) {
+	return "hoge", nil
 }
